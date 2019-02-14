04340950834f29fd712af0eb0d945373feedbc18
Ben Leung <benjamin.leung80@gmail.com>
2019-02-14 10:28:30 +0800

    Add files via upload

--- Tutorial Output/Tutorial 2/main.py
+++ Tutorial Output/Tutorial 2/main.py
@@ -1,13 +1,13 @@
 # Tutorial 2.1
 # CircuitPython demo - NeoPixel
- 
+
 import time
- 
+
 import board
 import neopixel
- 
-pixpin = board.D0
-numpix = 64
+
+pixpin = board.A3
+numpix = 128
 
 strip = neopixel.NeoPixel(pixpin, numpix, brightness=0.1, auto_write=False)
 
@@ -19,23 +19,23 @@ while True:
 
     strip[0] = (255,0,0)
     strip.write()
-    time.sleep(1)
+    time.sleep(0)
 
     strip[1] = (255,0,0)
     strip.write()
-    time.sleep(1)
+    time.sleep(0)
 
     strip[2] = (255,0,0)
     strip.write()
-    time.sleep(1)
+    time.sleep(0)
 
     strip[3] = (255,0,0)
     strip.write()
-    time.sleep(1)
+    time.sleep(2)
 
     strip[0] = (0,0,0)
     strip[1] = (0,0,0)
     strip[2] = (0,0,0)
     strip[3] = (0,0,0)
     strip.write()
-    time.sleep(1)
+    time.sleep(2)
