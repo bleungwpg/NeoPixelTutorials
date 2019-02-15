8b8f638c606ea731760a3827e0982e14111ac8ac
Ben Leung <benjamin.leung80@gmail.com>
2019-02-15 12:53:16 +0800

    Add files via upload

--- Tutorial Output/Tutorial 4/main.py
+++ Tutorial Output/Tutorial 4/main.py
@@ -1,40 +1,38 @@
 # Tutorial 4.1
 # CircuitPython demo - NeoPixel
- 
+
 import time
 import board
 import neopixel
- 
-pixpin = board.D0
-numpix = 64
+
+pixpin = board.A3
+numpix = 128
 
 strip = neopixel.NeoPixel(pixpin, numpix, brightness=0.1, auto_write=False)
 
 while True:
 
-    strip.brightness = 0.4
-
-    # set color to red
-    strip[0] = (255,0,0)
+    strip[0] = (50,0,0)
     strip.write()
     time.sleep(1)
 
-    # set color to green
-    strip[0] = (0,255,0)
+    strip[1] = (100,0,0)
     strip.write()
     time.sleep(1)
 
-    # set color to blue
-    strip[0] = (0,0,255)
+    strip[2] = (150,0,0)
     strip.write()
     time.sleep(1)
 
-    # set color to orange
-    strip[0] = (125,255,0)
+    strip[3] = (200,0,0)
     strip.write()
     time.sleep(1)
 
-    # set color to light blue
-    strip[0] = (102,228,247)
+    strip[4] = (255,0,0)
     strip.write()
     time.sleep(1)
+
+    for x in range(0,5)
+        strip[x] = (0,0,0)
+    strip.write()
+    time.sleep(2)
